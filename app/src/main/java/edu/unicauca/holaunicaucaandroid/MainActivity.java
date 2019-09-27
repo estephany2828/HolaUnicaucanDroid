2c483cf6a9e1ee57ff2036d6dab4069e0f464bc6
Johana2828 <ejargoty@unicauca.edu.co>
2019-09-26 19:32:50 -0500

    Initial commit

---
+++ app/src/main/java/edu/unicauca/holaunicaucaandroid/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.unicauca.holaunicaucaandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Date;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+
+        super.onCreate(icicle);
+        //TODO: Falta hacer la actividad 1
+
+        setContentView(R.layout.activity_main);
+        btn = new Button(this);
+        btn.setOnClickListener(this);
+        updateTime();
+        setContentView(btn);
+    }
+
+    private void updateTime() {
+        btn.setText((new Date()).toString());
+    }
+
+    @Override
+    public void onClick(View view) {
+        updateTime();
+    }
+}
